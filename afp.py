8ec0755c931706b90781a9e650309eb85e08fcfb
FrosstyCl <frosty98.pmo@gmail.com>
2019-03-21 19:59:03 -0300

    Update afp.py

--- afp.py
+++ afp.py
@@ -1,13 +1,14 @@
 from tkinter import *
 from tkinter import ttk
 
-ventana=Tk()
-ventana.geometry('400x400') #ancho x alto
-ventana.title('Retirement Simulator')
-ttk.Button(ventana,text='Salir',command=quit).pack(side=BOTTOM)
+class Application():
+	def __init__(self):
+		window=Tk()
+		window.geometry('400x400')
+		window.configure(wh='white')
+		window.title('Retirement Simulator')
+		ttk.Button(window,text='Salir',command=window.destroy).pack(side=BOTTOM)
+		window.mainloop()
 
-
-
-
-ventana.mainloop()
+mi_app=Application()
 
