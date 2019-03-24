623f5d33e45608727f58b966aad2de308281454a
FrosstyCl <frosty98.pmo@gmail.com>
2019-03-24 05:06:18 -0300

    Update afp.py

--- afp.py
+++ afp.py
@@ -16,6 +16,8 @@ class Application:
 		self.Sueldo=IntVar()
 		self.SaldoInicial=IntVar()
 		self.Edad=IntVar()
+		self.Rentabilidad=IntVar()
+		self.APV=IntVar()
 		self.Start()
 
 	def Start(self):
@@ -45,40 +47,7 @@ class Application:
 		self.afp=Frame(self.window)
 		self.afp.pack()
 		self.inicio=True
-		Label(self.afp,text="Selecciona la AFP donde cotices.").grid(row=0,column=0,rowspan=2,columnspan=3)
-		Button(self.afp,text="Cuprum",font=("Arial",12),command=self.cuprum).grid(row=2,column=0,padx=20,pady=20)
-		Button(self.afp,text="Modelo",font=("Arial",12),command=self.modelo).grid(row=2,column=1,padx=20,pady=20)
-		Button(self.afp,text="Habitat",font=("Arial",12),command=self.habitat).grid(row=2,column=2,padx=20,pady=20)
-		Button(self.afp,text="PlanVital",font=("Arial",12),command=self.plan).grid(row=3,column=0,padx=20,pady=20)
-		Button(self.afp,text="Capital",font=("Arial",12),command=self.capital).grid(row=3,column=1,padx=20,pady=20)
-		Button(self.afp,text="Provida",font=("Arial",12),command=self.provida).grid(row=3,column=2,padx=20,pady=20)
-		Button(self.afp,text="volver",command=self.Start).grid(row=5,column=0,padx=20,pady=20)
-		Button(self.afp,text="Salir",font=("Arial",12),command=self.salir).grid(row=5,column=2)
-
-	def Deseado(self):
-		self.frame.destroy()
-		self.afp=Frame(self.window)
-		self.afp.pack()
-		self.inicio=True
-		Label(self.afp,text="Selecciona la AFP donde cotices.").grid(row=0,column=0,rowspan=2,columnspan=3)
-		Button(self.afp,text="Cuprum",font=("Arial",12),command=self.cuprum).grid(row=2,column=0,padx=20,pady=20)
-		Button(self.afp,text="Modelo",font=("Arial",12),command=self.modelo).grid(row=2,column=1,padx=20,pady=20)
-		Button(self.afp,text="Habitat",font=("Arial",12),command=self.habitat).grid(row=2,column=2,padx=20,pady=20)
-		Button(self.afp,text="PlanVital",font=("Arial",12),command=self.plan).grid(row=3,column=0,padx=20,pady=20)
-		Button(self.afp,text="Capital",font=("Arial",12),command=self.capital).grid(row=3,column=1,padx=20,pady=20)
-		Button(self.afp,text="Provida",font=("Arial",12),command=self.provida).grid(row=3,column=2,padx=20,pady=20)
-		Button(self.afp,text="volver",command=self.Start).grid(row=5,column=0,padx=20,pady=20)
-		Button(self.afp,text="Salir",font=("Arial",12),command=self.salir).grid(row=5,column=2)
-
-	def cuprum(self):
-		self.frame.destroy()
-		self.afp.destroy()
-		self.inicio=True
-		self.afp=Frame(self.window,bg="beige")
-		self.afp.pack()
-		
-		#imagen_cuprum=PhotoImage(file="AFP.png")
-		Label(self.afp,text="Cotizando en Cuprum",font=("Arial",16)).grid(row=0,column=0,columnspan=3)
+		Label(self.afp,text="A continuacion ingresa tus datos",font=("Arial",16)).grid(row=0,column=0,columnspan=3)
 		Label(self.afp,text="Edad :").grid(row=2,column=0)
 		Entry(self.afp,textvariable=self.Edad).grid(row=2,column=1,columnspan=2)
 
@@ -92,36 +61,59 @@ class Application:
 		Label(self.afp,text="Saldo Inicial: ").grid(row=5,column=0)
 		Entry(self.afp,textvariable=self.SaldoInicial).grid(row=5,column=1,columnspan=2)
 
-		Button(self.afp,text="Calcular",command=self.Calculo).grid(row=8,column=2)
+		Label(self.afp,text="AFP: ").grid(row=6,column=0,rowspan=2)
+		#RECORDATORIO CAMBIAR LAS RENTABILIDADES
+		Radiobutton(self.afp,text="Cuprum",value=1,variable=self.Rentabilidad).grid(row=6,column=1)
+		Radiobutton(self.afp,text="Modelo",value=2,variable=self.Rentabilidad).grid(row=6,column=2)
+		Radiobutton(self.afp,text="PlanVital",value=3,variable=self.Rentabilidad).grid(row=6,column=3)
+		Radiobutton(self.afp,text="Provida",value=4,variable=self.Rentabilidad).grid(row=7,column=1)
+		Radiobutton(self.afp,text="Habitat",value=5,variable=self.Rentabilidad).grid(row=7,column=2)
+		Radiobutton(self.afp,text="Capital",value=6,variable=self.Rentabilidad).grid(row=7,column=3)
+
+		Button(self.afp,text="Calcular",command=self.Calculo_Esperado).grid(row=8,column=2)
 		Button(self.afp,text="volver",command=self.Start).grid(row=8,column=0,columnspan=2,padx=20,pady=20)
-		
-	def modelo(self):
-		self.afp.destroy()
+
+	def Deseado(self):
 		self.frame.destroy()
-		self.inicio=True
-		self.afp=Frame(self.window,bg="beige")
+		self.afp=Frame(self.window)
 		self.afp.pack()
-		Label(self.afp,text="Cotizando en",font=("Arial",16)).grid(row=0,column=0,columnspan=3)
-		Button(self.afp,text="volver",command=self.Start).grid(row=5,column=1,columnspan=3,padx=20,pady=20)
-		Entry(self.afp,textvariable=self.Edad).grid(row=2,column=2)
-		Label(self.afp,text="Edad :").grid(row=2,column=1)
-		Entry(self.afp,textvariable=self.Sueldo).grid(row=3,column=2)
-		Label(self.afp,text="Sueldo :").grid(row=3,column=1)
+		self.inicio=True
 
+		Label(self.afp,text="A continuacion ingresa tus datos",font=("Arial",16)).grid(row=0,column=0,columnspan=3)
+		Label(self.afp,text="Edad :").grid(row=2,column=0)
+		Entry(self.afp,textvariable=self.Edad).grid(row=2,column=1,columnspan=2)
 
-	def habitat(self):
-		pass
-	def plan(self):
-		pass
-	def capital(self):
-		pass
-	def provida(self):
-		pass
+		Label(self.afp,text="Sueldo :").grid(row=3,column=0)
+		Entry(self.afp,textvariable=self.Sueldo).grid(row=3,column=1,columnspan=2)		
+		
+		Label(self.afp,text="Sexo: ").grid(row=4,column=0)
+		Radiobutton(self.afp,text="Hombre",value=1,variable=self.Sexo).grid(row=4,column=1)
+		Radiobutton(self.afp,text="Mujer",value=2,variable=self.Sexo).grid(row=4,column=2)
+		
+		Label(self.afp,text="Saldo Inicial: ").grid(row=5,column=0)
+		Entry(self.afp,textvariable=self.SaldoInicial).grid(row=5,column=1,columnspan=2)
+
+		Label(self.afp,text="AFP: ").grid(row=6,column=0,rowspan=2)
+		#RECORDATORIO CAMBIAR LAS RENTABILIDADES
+		Radiobutton(self.afp,text="Cuprum",value=1,variable=self.Rentabilidad).grid(row=6,column=1)
+		Radiobutton(self.afp,text="Modelo",value=2,variable=self.Rentabilidad).grid(row=6,column=2)
+		Radiobutton(self.afp,text="PlanVital",value=3,variable=self.Rentabilidad).grid(row=6,column=3)
+		Radiobutton(self.afp,text="Provida",value=4,variable=self.Rentabilidad).grid(row=7,column=1)
+		Radiobutton(self.afp,text="Habitat",value=5,variable=self.Rentabilidad).grid(row=7,column=2)
+		Radiobutton(self.afp,text="Capital",value=6,variable=self.Rentabilidad).grid(row=7,column=3)
+
+		Label(self.afp,text="Monto deseado: ").grid(row=8,column=0)
+		Entry(self.afp,textvariable=self.APV).grid(row=8,column=1,columnspan=2)
+
+		Button(self.afp,text="Calcular",command=self.Calculo_Deseado).grid(row=9,column=2)
+		Button(self.afp,text="volver",command=self.Start).grid(row=9,column=0,columnspan=2,padx=20,pady=20)
+		
+
 	def salir(self):
 		answer=messagebox.askquestion("Salir","¿Deseas cerrar la aplicacion?")
 		if answer=="yes":
 			self.window.destroy()
-	def Calculo(self,rentabilidad=0.07):
+	def Calculo_Esperado(self):
 		self.frame.destroy()
 		self.afp.destroy()
 		self.inicio=True
@@ -136,7 +128,7 @@ class Application:
 			Tiempo=60-self.Edad.get()
 		Deposito=self.Sueldo.get()*1200
 		print(Tiempo,Deposito)
-		self.Total=self.SaldoInicial.get()*((1+rentabilidad)**Tiempo)+(Deposito*((1+rentabilidad)**(Tiempo-1))/rentabilidad)
+		self.Total=self.SaldoInicial.get()*((1+self.Rentabilidad.get())**Tiempo)+(Deposito*((1+self.Rentabilidad.get())**(Tiempo-1))/self.Rentabilidad.get())
 		
 		Label(self.afp,text="Tu total es: "+ str(self.Total)).grid(row=1,column=0,columnspan=3)
 		if self.Sexo==1:#Hombre
@@ -144,10 +136,34 @@ class Application:
 		else:
 			Label(self.afp,text="Tu pension llegaria a : "+str(int(self.Total/300))+" mil pesos aprox").grid(row=2,column=0,columnspan=3)
 				
-		Label(self.afp,text="Total-1: "+str(self.Total-1)).grid(row=3,column=1)
+		#Label(self.afp,text="Total-1: "+str(self.Total-1)).grid(row=3,column=1)
 		
+	def Calculo_Deseado(self):
+		self.frame.destroy()
+		self.afp.destroy()
+		self.inicio=True
+		self.afp=Frame(self.window,bg="beige")
+		self.afp.pack()
+
+		Label(self.afp,text="Resultados").grid(row=0,column=0,columnspan=2)
+		
+		if self.Sexo==1:#Hombre
+			Tiempo=65-self.Edad.get()
+		else:#Mujer
+			Tiempo=60-self.Edad.get()
+		Deposito=self.Sueldo.get()*1200
+		self.Total=self.SaldoInicial.get()*((1+self.Rentabilidad.get())**Tiempo)+(Deposito*((1+self.Rentabilidad.get())**(Tiempo-1))/self.Rentabilidad.get())
+		
+		Label(self.afp,text="Tu total actual es: "+ str(self.Total)).grid(row=1,column=0,columnspan=3)
 
 
+
+		
+		if self.Sexo==1:#Hombre,180
+			faltante=self.APV.get()*180-self.Total
+
+		else:#mujeres,300
+			Label(self.afp,text="Tu pension llegaria a : "+str(int(self.Total/300))+" mil pesos aprox").grid(row=2,column=0,columnspan=3)
 		
 
 
